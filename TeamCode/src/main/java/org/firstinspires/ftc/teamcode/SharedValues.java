765dd92786827468307ed26871f79ddb78663e42
Scott Buchanan <buchananscc@posteo.net>
2024-06-04 21:23:55 +0100

    enter measurements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedValues.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedValues.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode;
 public class SharedValues {
     public static final int MILLISECONDS_PER_VERTICAL_TILE = 1205;
     public static final int MILLISECONDS_PER_HORIZONTAL_TILE = 1548;
-    public static final int WHEEL_BASE = 0; // millimetres TODO
-    public static final int AXLE_TRACK = 0; // millimetres TODO
+    public static final int WHEEL_BASE = 335; // millimetres
+    public static final int AXLE_TRACK = 395; // millimetres
 
     public static boolean approxEquals(double a, double b, double tolerance) {
         return Math.abs(a - b) < tolerance;

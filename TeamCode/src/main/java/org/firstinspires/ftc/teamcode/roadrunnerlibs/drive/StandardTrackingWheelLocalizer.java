a38677c1a1abccea5daf9f055c053aaa36ed432f
Scott Buchanan <buchananscc@posteo.net>
2024-06-13 18:10:15 +0100

    new localisation values/coefficients

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunnerlibs/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunnerlibs/drive/StandardTrackingWheelLocalizer.java
@@ -33,7 +33,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1.1811023622; // in (30mm)
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 16; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 5.2; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -6; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1;
